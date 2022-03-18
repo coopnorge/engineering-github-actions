3baa8b2847884d3139480f3a677c42b92b5d53d6
Victor Nascimento Bakke <victor.bakke@coop.no>
2022-03-18 12:59:55 +0100

    separate golangci steps into separate actions

---
+++ .github/actions/create-golangci-config/index.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+
+const configs = ["yml", "yaml", "toml", "json"].map((x) => `./.golangci.${x}`);
+
+configs.forEach((config) => {
+  if (fs.existsSync(config) && fs.lstatSync(config).isFile()) {
+    console.error(`Found existing config: ${config}`);
+    process.exit(0);
+  }
+});
+
+console.error(`Config not found. Writing default config to ${configs[0]}`);
+const defaultConfig = fs.readFileSync("./default_config.yaml", "utf8");
+fs.writeFileSync(configs[0], defaultConfig, "utf8");
